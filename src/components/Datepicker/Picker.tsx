55ad1f37d2356f4b5fc01059c729774b43658486
Phil Scott <philscott.dev@gmail.com>
2020-07-18 21:28:42 -0400

    moved picker position

--- src/components/Datepicker/Picker.tsx
+++ src/components/Datepicker/Picker.tsx
@@ -3,8 +3,8 @@ import styled from '@emotion/styled'
 const Picker = styled.div<{ isVisible: boolean }>`
   position: absolute;
   visibility: ${({ isVisible }) => (isVisible ? 'visible' : 'hidden')};
-  left: 100%;
-  top: 0;
+  left: 0;
+  top: 100%;
   padding: 8px;
   border-radius: 2px;
   box-shadow: 20px 20px 40px #050611;
