4519e96ff303a89075afa098b6b61ae06b89b7c1
Phil Scott <philscott.dev@gmail.com>
2020-07-25 09:47:01 -0400

    linting

--- src/lib/Planner/hooks/useFilteredEvents.ts
+++ src/lib/Planner/hooks/useFilteredEvents.ts
@@ -6,21 +6,23 @@ import { PlannerEventGroup } from '../types'
  * sets and checks the multirow layout, per user
  */
 export default function usePlannerEventRow(
-  groups: PlannerEventGroup[],
-  range: Date[],
+  groups?: PlannerEventGroup[],
+  range?: Date[],
 ) {
   const [filteredGroups, setGroups] = useState<PlannerEventGroup[]>([])
 
   useEffect(() => {
-    const dateRange = { start: range[0], end: range[range.length - 1] }
-    const filtered = groups.map((groups) => {
-      const events = groups.events.filter((event) => {
-        const eventRange = { start: event.startTime, end: event.endTime }
-        return areIntervalsOverlapping(eventRange, dateRange)
+    if (groups && range) {
+      const dateRange = { start: range[0], end: range[range.length - 1] }
+      const filtered = groups.map((groups) => {
+        const events = groups.events.filter((event) => {
+          const eventRange = { start: event.startTime, end: event.endTime }
+          return areIntervalsOverlapping(eventRange, dateRange)
+        })
+        return { ...groups, events }
       })
-      return { ...groups, events }
-    })
-    setGroups(filtered)
+      setGroups(filtered)
+    }
   }, [groups, range])
   return filteredGroups
 }
